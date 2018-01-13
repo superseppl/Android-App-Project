67410d10839c10fdaaa94ced8b7f1774e036b0eb
ga58tet Bader Sebastian <se.bader@tum.de>
2018-01-13 16:23:40 +0100

    Hide the Status Bar in both Layouts

--- Base/app/src/main/java/com/projecttango/examples/java/augmentedreality/AugmentedRealityActivity.java
+++ Base/app/src/main/java/com/projecttango/examples/java/augmentedreality/AugmentedRealityActivity.java
@@ -31,6 +31,7 @@ import com.google.atap.tangoservice.TangoXyzIjData;
 import com.google.tango.support.TangoSupport;
 
 import android.Manifest;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -100,6 +101,15 @@ public class AugmentedRealityActivity extends Activity implements View.OnTouchLi
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Intent intent = getIntent();
+
+        // Hide the status bar.
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ar);
--- Base/app/src/main/java/com/projecttango/examples/java/augmentedreality/MainActivity.java
+++ Base/app/src/main/java/com/projecttango/examples/java/augmentedreality/MainActivity.java
@@ -1,5 +1,6 @@
 package com.projecttango.examples.java.augmentedreality;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,6 +12,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Hide the status bar.
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
     }
 
     public void sendStart(View view) {
