1b2e5352a8336fbd7734e35b344a95a2d21b5453
ga58mus <alek.pikl@tum.de>
2018-02-04 20:28:37 +0100

    Added Voice Parser. TODO: add callbacks. TODO: add browser with Planet infos

--- Base/app/src/main/java/com/projecttango/examples/java/augmentedreality/Voice.java
+++ Base/app/src/main/java/com/projecttango/examples/java/augmentedreality/Voice.java
@@ -2,10 +2,6 @@ package com.projecttango.examples.java.augmentedreality;
 
 import android.util.Log;
 
-/**
- * Created by alek on 03/02/2018.
- */
-
 public class Voice {
 
     static String TAG = "Voice";
